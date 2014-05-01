a9ae3c7d2977c20a461b87263fd977392a5de384
Jonas Konrad <me@yawk.at>
2014-05-01 18:55:07 +0200

    Add javadoc for @Init

--- src/main/java/at/yawk/felix/module/Init.java
+++ src/main/java/at/yawk/felix/module/Init.java
@@ -6,6 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Methods in modules that are annotated with this will be run when their module is initialized.
+ *
  * @author Jonas Konrad (yawkat)
  */
 @Target(ElementType.METHOD)
